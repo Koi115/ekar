e518ac30f124baed8733b29ebd57583c0dc8232b
Koi115 <chiiuko115@gmail.com>
2023-06-15 23:14:57 +0900

    患者詳細画面の年齢を自動計算

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,10 +1,10 @@
 class Ward < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '外来' },
-    { id: 3, name: '1病棟' },
-    { id: 4, name: '2病棟' },
-    { id: 9, name: '入院待ち' }
+    { id: 1, name: '---', max_num_beds: '0' },
+    { id: 2, name: '外来', max_num_beds: '0' },
+    { id: 3, name: '1病棟', max_num_beds: '50' },
+    { id: 4, name: '2病棟', max_num_beds: '40' },
+    { id: 9, name: '入院待ち', max_num_beds: '0' }
   ]
 
   include ActiveHash::Associations
---
+++ test/controllers/pending_admission_requests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PendingAdmissionRequestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
